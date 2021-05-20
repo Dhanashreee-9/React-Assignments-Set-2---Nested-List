47840de7700159eaf97cd265f6432792d4933485
Dhanashreee-9 <68267346+Dhanashreee-9@users.noreply.github.com>
2021-05-20 13:23:32 +0530

    Update Towns.js

--- src/components/Towns.js
+++ src/components/Towns.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Towns = (props) => {
     return (
-        <>
+        <React.Fragment>
              <li id={`town${props.id}`}>
                 <button>{props.town.name} </button>
             </li>
-        </>
+        </React.Fragment>
     )
 }
 
